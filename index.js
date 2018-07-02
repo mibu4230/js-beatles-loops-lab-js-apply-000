c7ab9c8dccc1090dc93e9c03c1303a6fadf6ee8f
Michelle Burke <micheburke17@yahoo.com>
2018-07-02 00:02:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var musicianArray = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instrumentArray = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+var i;
+   for (i = 0; i = musicianArray.Length; i = instrumentArray.Length; i++); {
+   
+   musician[i] + "plays" + instrument[i];
+   
+   }
+  function theBeatlesPlay (musicianArray, instrumentArray) {Console.log();
+  }
